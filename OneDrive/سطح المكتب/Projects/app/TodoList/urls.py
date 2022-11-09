f9a11114ccc3bc7721b28caf59495c25c4bf022b
tvvshehri <74836451+tvvshehri@users.noreply.github.com>
2022-11-09 22:08:53 +0300

    Update urls.py

--- OneDrive/سطح المكتب/Projects/app/TodoList/urls.py
+++ OneDrive/سطح المكتب/Projects/app/TodoList/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('done/<str:pk>', views.done, name='done')
+    path('', views.Home, name='Home'),
+    path('Delete/<str:pk>', views.Delete, name='Delete'),
+    path('Done/<str:pk>', views.Done, name='Done'),
+    path('Active/<str:pk>', views.Active, name='Active')
 ]
