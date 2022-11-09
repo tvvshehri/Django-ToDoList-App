a45e16394039d9bf969b9a81a05b5ae3b2647983
tvvshehri <74836451+tvvshehri@users.noreply.github.com>
2022-11-09 22:09:38 +0300

    Update views.py

--- OneDrive/سطح المكتب/Projects/app/TodoList/views.py
+++ OneDrive/سطح المكتب/Projects/app/TodoList/views.py
@@ -1,17 +1,29 @@
 from django.shortcuts import render, redirect
 from .models import Base
 
-# Create your views here.
-def home(request):
+def Home(request):
     todo_list = Base.objects.all()
     if request.method == 'POST':
         new_todolist = Base(task = request.POST['title'])
         new_todolist.save()
-        return redirect('/')
+        return redirect('Home')
 
     return render(request, 'home.html', {'todos': todo_list})
 
-def done(request, pk):
+def Delete(request, pk):
     todo_list = Base.objects.get(id=pk)
     todo_list.delete()
-    return redirect('/')
+    return redirect('Home')
+
+def Done(request, pk):
+    todo_list = Base.objects.get(id=pk)
+    todo_list.completed= True
+    todo_list.save()
+    return redirect('Home')
+
+def Active(request, pk):
+    todo_list = Base.objects.get(id=pk)
+    todo_list.completed= False
+    todo_list.save()
+    return redirect('Home')
+
