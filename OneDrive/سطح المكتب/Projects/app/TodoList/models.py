225a4f53266eba093b505114ac55efac569fc633
tvvshehri <74836451+tvvshehri@users.noreply.github.com>
2022-11-09 22:08:11 +0300

    Update models.py

--- OneDrive/سطح المكتب/Projects/app/TodoList/models.py
+++ OneDrive/سطح المكتب/Projects/app/TodoList/models.py
@@ -4,9 +4,10 @@ from django.db import models
 # Create your models here.
 class Base(models.Model):
     task = models.CharField(max_length=1000)
+    completed= models.BooleanField(default=False)
 
     def __str__(self):
-        return self.task
+        return self.task + ' | ' + str(self.completed)
 
 
 
